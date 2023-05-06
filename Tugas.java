0d9b753a48038438f4942856349b35efd4975d9b
Ivander Destian Luis <ivanderluis7@gmail.com>
2023-05-06 15:16:58 +0700

    Tambah File Baru

---
+++ Tugas.java
@@ -0,0 +1,12 @@
+public class Tugsa{
+    private String npm;
+
+    public String getNpm() {
+        return this.npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+}
